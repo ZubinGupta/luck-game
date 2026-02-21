084158fc5788714677421d0d85dd5ce315f6f2d7
Zubin Gupta <zubin.gupta.mail@gmail.com>
2026-02-21 17:22:31 -0500

    d

--- luck-game/weapons/rocketAttack.gd
+++ luck-game/weapons/rocketAttack.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
 var explosion_scene: PackedScene = preload("res://weapons/explosion.tscn")
-var special: bool = false
 
 func _physics_process(delta: float) -> void:
 	velocity = basis.z.normalized() * 50
@@ -9,7 +8,6 @@ func _physics_process(delta: float) -> void:
 	
 func kaboom(hit: Node3D):
 	var explosion = explosion_scene.instantiate()
-	get_tree().current_scene.add_child(explosion)
 	explosion.global_position = global_position
-	explosion.special = special
+	get_tree().current_scene.add_child(explosion)
 	queue_free()
