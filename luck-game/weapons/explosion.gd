3c00fa0a15e38296b5509b5a218d48e05a8903ea
snomkip7 <snomkip7@gmail.com>
2026-02-21 22:29:52 -0500

    Revert "d"
    This reverts commit 084158fc5788714677421d0d85dd5ce315f6f2d7.

--- luck-game/weapons/explosion.gd
+++ luck-game/weapons/explosion.gd
@@ -1,10 +1,12 @@
 extends Area3D
 @onready var lifespan = $Lifespan
 
+var special: bool = false
+
 func _ready() -> void:
 	lifespan.start()
 	await get_tree().physics_frame
-	await get_tree().physics_frame #idk 
+	
 	var bodies = get_overlapping_bodies()
 	print(bodies)
 	for body in bodies:
@@ -13,4 +15,12 @@ func _ready() -> void:
 
 
 func _on_lifespan_timeout() -> void:
-	queue_free()
+	if(is_inside_tree()):
+		queue_free()
+
+
+func onHit(_body: Node3D):
+	if(special):
+		$"../Player".knockbackVelocity = Vector3(0, 60, 0)
+	else:
+		$"../Player".knockbackVelocity = ($"../Player".global_position-global_position).normalized()*40*Vector3(1,0,1) + Vector3(0, 20, 0)
