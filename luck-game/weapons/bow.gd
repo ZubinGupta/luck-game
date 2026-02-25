9f96d575d75f1274f83867227f8130bd7371480a
Zubin Gupta <zubin.gupta.mail@gmail.com>
2026-02-24 20:54:23 -0500

    bow shotgun ability

--- luck-game/weapons/bow.gd
+++ luck-game/weapons/bow.gd
@@ -1,22 +1,27 @@
 extends CharacterBody3D
 
 var attack: PackedScene = preload("res://weapons/arrow.tscn")
-var arrowCooldown := false
-var arrows := 50;
 
 func _physics_process(delta: float) -> void:
 	
 	rotate_x(basis.z.signed_angle_to(-$"../PlayerCamera".basis.z, Vector3.RIGHT))
 	
 
-	if Input.is_action_just_pressed("attack") && arrowCooldown == false && arrows > 0:
+	if Input.is_action_just_pressed("attack") && $ArrowCooldown.is_stopped():
 		var arrow = attack.instantiate()
 		get_tree().current_scene.add_child(arrow)
 		arrow.look_at((get_parent().transform.basis*transform.basis).z*-1)
 		arrow.global_position = global_position
 		$ArrowCooldown.start();
-		arrowCooldown = true;
-		arrows-=1
-
-func _on_arrow_cooldown_timeout() -> void:
-	arrowCooldown = false
+	
+	if Input.is_action_just_pressed("special") && $SpecialCooldown.is_stopped():
+		for i in range(-1,2):
+			for j in range(-1,2):
+				var arrow = attack.instantiate()
+				get_tree().current_scene.add_child(arrow)
+				var spread = Vector3(i * 0.06, j * 0.06, 0)
+				var look_direction = (get_parent().transform.basis*transform.basis).z*-1
+				var final_direction = (look_direction + spread).normalized()
+				arrow.look_at(final_direction)
+				arrow.global_position = global_position
+		$SpecialCooldown.start();
